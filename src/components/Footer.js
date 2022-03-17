890ff1609eec3332154791eeebfcc8932d28c7d5
Tnaike <timmynaike@gmail.com>
2022-03-17 22:16:45 +0100

    add footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './style.css';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <footer>
+      <p className='m-0'>
+        Copyright ©2022{' '}
+        <NavLink className='text-white' to='/'>
+          impex.com.ng
+        </NavLink>{' '}
+        All rights reserved
+      </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
