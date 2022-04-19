fd3da085d5d19e75552b8ff026fde8a4543dafff
Tnaike <timmynaike@gmail.com>
2022-04-19 23:29:20 +0100

    create search panel

---
+++ src/components/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPage = () => {
+  return (
+    <>
+      <div className="search-panel-wrap"></div>
+    </>
+  );
+};
+
+export default SearchPage;
--- src/components/buttons/SearchButton.js
+++ src/components/buttons/SearchButton.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { ReactComponent as SearchIcon } from '../../assets/Search.svg';
+import SearchPage from '../SearchPage';
 
 const SearchButton = () => {
+  const [openSearchModal, setOpenSearchModal] = useState(false);
+console.log(openSearchModal);
   return (
     <>
-      <NavLink to='/' className='btn btn-cart a-link pr p-0 ms-3'>
+      <NavLink
+        to='/'
+        className='btn btn-cart a-link pr p-0 ms-3'
+        onClick={() => setOpenSearchModal(!openSearchModal)}
+      >
         <SearchIcon className='svg-navIcon' />
       </NavLink>
+
+      {openSearchModal && (
+        <SearchPage
+          showSearchModal={openSearchModal}
+          setShowSearchModal={setOpenSearchModal}
+        />
+      )}
     </>
   );
 };
