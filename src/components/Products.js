1be3af6ad5429abf7993f70997a098efa35bbacc
Tnaike <timmynaike@gmail.com>
2022-05-10 15:05:26 +0100

    configure environment from an env file

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 // import { fetchAllProducts } from '../redux/actions';
 import Skeleton from 'react-loading-skeleton';
 import { NavLink } from 'react-router-dom';
